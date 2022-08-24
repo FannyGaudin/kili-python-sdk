4c794e1055ce9047e62d51145d71261f11a50729
Pierre Leveau <pierre.leveau@kili-technology.com>
2022-08-24 13:25:54 +0200

    wip

--- kili/project/__init__.py
+++ kili/project/__init__.py
@@ -1,14 +1,14 @@
 """Project module."""
 
 from typing import List, Optional, cast
-from typing_extensions import Literal, NewType
-from kili.client import Kili
 
+from typing_extensions import Literal, NewType
 
 Format = Literal["coco", "voc", "yolo"]
 InputType = Literal["TEXT", "IMAGE"]
 AssetId = NewType("AssetId", str)
 ProjectId = NewType("ProjectId", str)
+
 
 class Project:  # pylint: disable=too-few-public-methods
     """
@@ -18,15 +18,17 @@ class Project:  # pylint: disable=too-few-public-methods
     It also allows queries from this project such as its assets, labels etc.
     """
 
-    def __init__(self, client: Kili, project_id: ProjectId, input_type: InputType, title: str):
+    def __init__(self, project_id: ProjectId, input_type: InputType, title: str):
         self.project_id = project_id
-        self.client = client
         self.title = title
         self.input_type = input_type
 
-    def export(self, path_output: str, format: Format, asset_ids: Optional[List[AssetId]]) -> None:
-        assets = self.client.assets(project_id=self.project_id, asset_id_in=cast(Optional[List[str]], asset_ids))
+    def export(
+        self, client, path_output: str, format: Format, asset_ids: Optional[List[AssetId]]
+    ) -> None:
+        _ = client.assets(
+            project_id=self.project_id, asset_id_in=cast(Optional[List[str]], asset_ids)
+        )
 
-        
-
-
+        if format == "yolo":
+            pass
