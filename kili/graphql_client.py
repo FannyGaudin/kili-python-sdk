775a23e99eee22c2a65bac1481672615dc7029d7
Paul Maisondieu <paul.maisondieu@kili-technology.com>
2022-09-02 23:24:30 +0200

    feat(qa_bot): add script_name arg

--- kili/graphql_client.py
+++ kili/graphql_client.py
@@ -85,9 +85,9 @@ class GraphQLClient:
         if self.session is not None:
             try:
                 try:
-                    number_of_trials = int(os.getenv("KILI_SDK_TRIALS_NUMBER", "10"))
+                    number_of_trials = int(os.getenv("KILI_SDK_TRIALS_NUMBER", "1"))
                 except ValueError:
-                    number_of_trials = 10
+                    number_of_trials = 1
                 for trial_number in range(number_of_trials):
                     self.session.verify = self.verify
                     req = self.session.post(
